2ef6c8857df1d354c1888e1cb99fcb00cd9ab01b
fuzz <fuzz@dazraf.io>
2021-12-03 11:08:50 +0000

    More compact form for Day2

--- src/main/kotlin/io/dazraf/aoc/y2021/Day02.kt
+++ src/main/kotlin/io/dazraf/aoc/y2021/Day02.kt
@@ -1,6 +1,8 @@
 package io.dazraf.aoc.y2021
 
-import io.dazraf.aoc.*
+import io.dazraf.aoc.Puzzle
+import io.dazraf.aoc.dataAsLines
+import io.dazraf.aoc.solve
 
 object Day02 : Puzzle(2021, 2, "Dive!") {
   @JvmStatic
@@ -9,50 +11,35 @@ object Day02 : Puzzle(2021, 2, "Dive!") {
   fun part1() = run(part1Strategy)
   fun part2() = run(part2Strategy)
 
-  private fun run(strategy: Strategy) = dataAsLines
-    .parseWith(strategy)
-    .fold(State()) { s, fn -> fn(s) }
-    .let { it.x * it.y }
+  private fun run(strategy: Map<String, (Int) -> (State) -> State>) =
+    dataAsLines
+      .parseWith(strategy)
+      .fold(State()) { s, fn -> fn(s) }
+      .let { it.x * it.y }
 
-  private val part1Strategy = Strategy(
-    forward = { amount -> { s -> s.copy(x = s.x + amount) } },
-    up = { amount -> { s -> s.copy(y = s.y - amount) } },
-    down = { amount -> { s -> s.copy(y = s.y + amount) } }
+  private const val FORWARD = "forward"
+  private const val UP = "up"
+  private const val DOWN = "down"
+
+  private val part1Strategy = mapOf<String, (Int) -> (State) -> State>(
+    FORWARD to { amount -> { s -> s.copy(x = s.x + amount) } },
+    UP to { amount -> { s -> s.copy(y = s.y - amount) } },
+    DOWN to { amount -> { s -> s.copy(y = s.y + amount) } }
   )
 
-  private val part2Strategy = Strategy(
-    forward = { amount -> { s -> s.copy(x = s.x + amount, y = s.y + s.aim * amount) } },
-    up = { amount -> { s -> s.copy(aim = s.aim - amount) } },
-    down = { amount -> { s -> s.copy(aim = s.aim + amount) } }
+  private val part2Strategy = mapOf<String, (Int) -> (State) -> State>(
+    FORWARD to { amount -> { s -> s.copy(x = s.x + amount, y = s.y + s.aim * amount) } },
+    UP to { amount -> { s -> s.copy(aim = s.aim - amount) } },
+    DOWN to { amount -> { s -> s.copy(aim = s.aim + amount) } }
   )
 
   private data class State(val x: Int = 0, val y: Int = 0, val aim: Int = 0)
 
-  /**
-   * A strategy is a set of functions, one per command type, that given an amount for the command,
-   * create a [State] evolution function ([State]) -> [State]
-   */
-  private data class Strategy(
-    val forward: (Int) -> (State) -> State,
-    val up: (Int) -> (State) -> State,
-    val down: (Int) -> (State) -> State
-  )
-
-  /**
-   * parse the instructions to generate a [Sequence] of [State] evolving functions ([State]) -> [State]
-   */
-  private fun Sequence<String>.parseWith(strategy: Strategy) = parsePairs().map { (command, amount) ->
-    when (command) {
-      "forward" -> strategy.forward(amount)
-      "down" -> strategy.down(amount)
-      "up" -> strategy.up(amount)
-      else -> error("unknown command $command")
+  private fun Sequence<String>.parseWith(strategy: Map<String, (Int) -> (State) -> State>) =
+    parsePairs().map { (command, amount) ->
+      strategy[command]?.invoke(amount) ?: error("unknown command $command")
     }
-  }
 
-  /**
-   * first stage of parsing to [Pair<String, Int>]
-   */
   private fun Sequence<String>.parsePairs() =
     this.map { line -> line.trim().split(' ').let { (c, a) -> c to a.toInt() } }
 }
