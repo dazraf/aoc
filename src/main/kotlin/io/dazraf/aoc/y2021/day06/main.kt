df7a8ba1aa04969e0c5f332083c0b6f48d7848ac
Fuzz <fuzz@dazraf.io>
2021-12-06 23:51:14 +0000

    Day 6 refactor.

--- src/main/kotlin/io/dazraf/aoc/y2021/day06/main.kt
+++ src/main/kotlin/io/dazraf/aoc/y2021/day06/main.kt
@@ -11,33 +11,29 @@ fun main() = Puzzle(2021, 6, "Lanternfish").solve(Puzzle::part1, Puzzle::part2)
 fun Puzzle.part1() = calculate(80)
 fun Puzzle.part2() = calculate(256)
 
-/**
- * @param population - map of remaining days on the timer to total fish population for that group
- * @param daysRemaining - number of simulation days remaining
- */
+fun Puzzle.calculate(totalDays: Int) =
+  generateSequence(State(initialPopulation(), totalDays), State::evolveOrNull).last().totalPopulation
+
+fun Puzzle.initialPopulation() =
+  dataList.first().toIntList().groupBy { it }.map { (days, fish) -> days to fish.size.toLong() }.toMap()
+
 class State(private val population: Map<Int, Long>, val daysRemaining: Int) {
   operator fun get(day: Int) = population[day] ?: 0
   val totalPopulation by lazy { population.values.sum() }
 }
 
-fun State.evolve() =
-  when (daysRemaining) {
-    0 -> null
-    else -> {
-      val newPopulation =
-        (0..newFishTimer).associateWith { daysLeft ->
-          when (daysLeft) {
-            resetFishTimer -> this[daysLeft + 1] + this[0]
-            newFishTimer -> this[0]
-            else -> this[daysLeft + 1]
-          }
-        }
-      State(newPopulation, daysRemaining - 1)
+fun State.evolveOrNull() = when (daysRemaining) {
+  0 -> null
+  else -> evolve()
+}
+
+private fun State.evolve() = State(
+  (0..newFishTimer).associateWith { daysLeft ->
+    when (daysLeft) {
+      resetFishTimer -> this[daysLeft + 1] + this[0]
+      newFishTimer -> this[0]
+      else -> this[daysLeft + 1]
     }
-  }
+  }, daysRemaining - 1
+)
 
-fun Puzzle.calculate(totalDays: Int) =
-  generateSequence(State(initialPopulation(), totalDays), State::evolve).last().totalPopulation
-
-fun Puzzle.initialPopulation() =
-  dataList.first().toIntList().groupBy { it }.map { (days, fish) -> days to fish.size.toLong() }.toMap()
