4dcea8d797341580393e011af7b1db909be86a1c
fuzz <fuzz@dazraf.io>
2021-12-07 10:35:39 +0000

    Day 7 tidy.

--- src/main/kotlin/io/dazraf/aoc/y2021/day07/main.kt
+++ src/main/kotlin/io/dazraf/aoc/y2021/day07/main.kt
@@ -10,10 +10,11 @@ fun main() = Puzzle(2021, 7, "The Treachery of Whales").solve(Puzzle::part1, Puz
 fun Puzzle.part1() = compute(::part1FuelCost)
 fun Puzzle.part2() = compute(::part2FuelCost)
 
-fun Puzzle.compute(fuelCalculator: (Int) -> Int) =
-  dataList.first().toIntList().let { crabs ->
+fun Puzzle.compute(fuelCalculator: (Int) -> Int) = readCrabs().let { crabs ->
     crabs.boundingRange().minOf { position -> crabs.sumOf { crab -> fuelCalculator(abs(crab - position)) } }
   }
 
 fun part1FuelCost(delta: Int) = delta
 fun part2FuelCost(delta: Int) = (delta * (delta + 1)) / 2 // simple nth partial sum of series
+
+fun Puzzle.readCrabs() = dataList.first().toIntList()
