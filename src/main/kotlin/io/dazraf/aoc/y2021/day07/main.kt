dcf44d3e01367141caba5126d62c58f5cc38e502
fuzz <fuzz@dazraf.io>
2021-12-07 11:30:30 +0000

    Day 7 tidy.

--- src/main/kotlin/io/dazraf/aoc/y2021/day07/main.kt
+++ src/main/kotlin/io/dazraf/aoc/y2021/day07/main.kt
@@ -11,8 +11,10 @@ fun Puzzle.part1() = compute(::part1FuelCost)
 fun Puzzle.part2() = compute(::part2FuelCost)
 
 fun Puzzle.compute(fuelCalculator: (Int) -> Int) = readCrabs().let { crabs ->
-    crabs.boundingRange().minOf { position -> crabs.sumOf { crab -> fuelCalculator(abs(crab - position)) } }
+  crabs.boundingRange().minOf { position ->
+    crabs.sumOf { crab -> fuelCalculator(abs(crab - position)) }
   }
+}
 
 fun part1FuelCost(delta: Int) = delta
 fun part2FuelCost(delta: Int) = (delta * (delta + 1)) / 2 // simple nth partial sum of series
